a2e50d6057434bcc6a7a8f52a6c6a0a0dcee8eba
f-gabriel-braga <braagaa.inf@hotmail.com>
2022-08-17 17:35:23 -0300

    Add home component

---
+++ src/app/shared/material/material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+@NgModule({
+  exports: [
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule
+  ]
+})
+export class MaterialModule { }
